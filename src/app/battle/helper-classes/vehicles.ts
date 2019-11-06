a524268434b30d6f8ec1a5ae0cf62289553b27ab
Maxim Baranov <maxim4220@gmail.com>
2019-11-06 09:22:50 +0200

    Removed interface

--- src/app/battle/helper-classes/vehicles.ts
+++ src/app/battle/helper-classes/vehicles.ts
@@ -1,20 +1,15 @@
 import {Units} from '../interfaces/units.interface';
 
-export interface Vehicles extends Units {
-  operators: number;
-}
-
-
-export class Vehicle implements Vehicles {
+export class Vehicle implements Units {
   // Vehicle recharge must not be less then 1 second.
-  recharge = Math.floor(Math.random() * 2000) + 1000;
+  recharge = 1;
   experience = 0;
   operators = 3;
   health = 100 + (this.operators * 100);
 
-
   calculateGeomMean(vehicle, operators) {
-    return 0.5 * (1 + vehicle.health / 100) * Math.pow(operators.reduce((a, b) => a * b), 1 / operators.length);
+    return 0.5 * (1 + vehicle.health / 100) * Math.pow(operators.reduce((a, b) => a * b), 1 / operators);
   }
 
 }
+
